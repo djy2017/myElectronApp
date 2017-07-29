79ca73117e04f934799c0c7cfb59521a5c230cd0
dengjianyuan <515746057@qq.com>
2017-07-29 15:05:53 +0800

    djy electron app

---
+++ renderer.js
@@ -0,0 +1,7 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+console.log(555);
+
+var shell = require('electron').shell;
+shell.openExternal('https://github.com');
